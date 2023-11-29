bbd0c2159883d291397908b4cfed6141945d3af4
LUEJ9 <juan.luethy@coop.ch>
2023-11-29 16:29:53 +0100

    Unfinished version (lombok error) last update 29.11.

---
+++ src/main/java/JavaList/Main.java
@@ -0,0 +1,18 @@
+package JavaList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+
+        List<String> studentList = new ArrayList<>();
+
+        studentList.add("Kuan");
+        studentList.add("Kebab");
+        studentList.add("Timio");
+
+
+
+    }
+}
---
+++ src/main/java/JavaList/Student.java
@@ -0,0 +1,31 @@
+package JavaList;
+
+public class Student {
+    private String name;
+    private String surname;
+    private int studentnumber;
+
+    public int getStudentnumber() {
+        return studentnumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setStudentnumber(int studentnumber) {
+        this.studentnumber = studentnumber;
+    }
+}
